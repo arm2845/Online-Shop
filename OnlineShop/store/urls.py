7299be0fe48efdebe1e5869797834eeacf0072d0
Armine <arminesargsyan1996@gmail.com>
2021-02-27 10:58:20 +0400

    27.02.2021

--- OnlineShop/store/urls.py
+++ OnlineShop/store/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.store, name='Store'),
     path('cart/', views.cart, name='Cart'),
+    path('update_item/', views.updateItem, name='UpdateItem'),
 ]
